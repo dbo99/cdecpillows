3152ffcb6cb38e071317e3e8b7198487f460aa14
dbo99 <oconnor.david.b@gmail.com>
2020-01-06 12:29:09 -0800

    Add files via upload

--- cdec_snow_scraper_build2.r
+++ cdec_snow_scraper_build2.r
@@ -1,4 +1,4 @@
-#rm(list = ls())
+rm(list = ls())
 #rstudioapi::getActiveDocumentContext
 #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
@@ -6,7 +6,7 @@ source("libs.r")
 source("fun_defs.r")
 
 
-page <- "http://cdec.water.ca.gov/reportapp/javareports?name=PAGE6"
+page <- "https://cdec.water.ca.gov/reportapp/javareports?name=PAGE6.20200102"
 
 
 start_date <- ymd("2019-11-26")
@@ -23,10 +23,11 @@ dates <- seq(start_date, end_date, by = "days") %>% rev()
   
 cdec_swe_table <- "http://cdec.water.ca.gov/reportapp/javareports?name=PAGE6.20191230"
 
-date <- mdy(html_text(html_node(read_html(page),"h2 , h2")), format = yyyymmdd) #.h2 & pre Id'd by SelectorGadget
+date <- mdy(html_text(html_node(read_html(page),"h2 , h2"))) #.h2 & pre Id'd by SelectorGadget
 
+date1 <- gsub("-","",date)
 
-cdec_swe_table  <- readHTMLTable(cdec_swe_table )
+cdec_swe_table  <- readHTMLTable(cdec_swe_table ) 
 
 
 cdec_swe_table_1 <- cdec_swe_table[[1]]
@@ -64,34 +65,20 @@ head(cdec_swe_table)
 #colnames <- colnames(cdec_swe_table)
 #colnames
 
-cdec_swe_table <- cdec_swe_table %>% transmute(Station, ID, oper, dwr_elev,
-                  lat, lon, dwrapr1mean, swe_latest = `Today (IN)`, 
+cdec_swe_table <- cdec_swe_table %>% transmute(Station, ID,
+                  dwrapr1mean = `Apr 1 Avg (IN)`, swe_latest = `Today (IN)`, 
                   swe_percapr1 = `Percent Apr 1`, swe_24hrprev = `24 Hrs Ago (IN)`, 
                   swe_7dayprev = `1 Week Ago (IN)`)
 
 head(cdec_swe_table)
 
-cdec_swe_table$swe_latest <- gsub("r", "", cdec_swe_table$swe_latest)
-cdec_swe_table$swe_latest <- gsub("e", "", cdec_swe_table$swe_latest)
-cdec_swe_table$swe_latest <- gsub("---", NA, cdec_swe_table$swe_latest)
 
-cdec_swe_table$swe_percapr1 <- gsub("%", "", cdec_swe_table$swe_percapr1)
-cdec_swe_table$swe_percapr1 <- gsub("---", NA, cdec_swe_table$swe_percapr1)
+cdec_swe_table2 <- cdec_swe_table %>% pivot_longer(3:7)
+head(cdec_swe_table2)
 
-cdec_swe_table$swe_24hrprev <- gsub("r", "",   cdec_swe_table$swe_24hrprev)
-cdec_swe_table$swe_24hrprev <- gsub("e", "",   cdec_swe_table$swe_24hrprev)
-cdec_swe_table$swe_24hrprev <- gsub("---", NA, cdec_swe_table$swe_24hrprev)
+cdec_swe_table2$value <- as.numeric(gsub("[^0-9.-]", "", cdec_swe_table2$value))
+head(cdec_swe_table2)
 
-cdec_swe_table$swe_7dayprev <- gsub("r", "",   cdec_swe_table$swe_7dayprev)
-cdec_swe_table$swe_7dayprev <- gsub("e", "",   cdec_swe_table$swe_7dayprev)
-cdec_swe_table$swe_7dayprev <- gsub("---", NA, cdec_swe_table$swe_7dayprev)
-
-cdec_swe_table <- cdec_swe_table %>% mutate(swe_percapr1 = as.numeric(swe_percapr1 ),
-                                            swe_latest = as.numeric(swe_latest ),
-                                             swe_24hrprev = as.numeric(swe_24hrprev ),
-                                              swe_7dayprev = as.numeric(swe_7dayprev))
-cdec_swe_table <- cdec_swe_table %>% mutate(swe_dailydiff =  swe_latest - swe_24hrprev,
-                                            swe_weeklydiff = swe_latest - swe_7dayprev)
 
 cdec_swe_table <- st_as_sf(cdec_swe_table, coords = c("lon", "lat"), crs = 4326) 
 
